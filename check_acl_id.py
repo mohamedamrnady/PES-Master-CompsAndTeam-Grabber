edf0e2f9697cc00d30aa35027834f2b5ce8d1152
Nady <mohamed.amr.nes@gmail.com>
2024-01-05 16:41:56 +0200

    Change ACL conditions again

--- check_acl_id.py
+++ check_acl_id.py
@@ -1,13 +1,16 @@
 def checkACLID(teamID: str):
     hexValue = hex(int(teamID))[2:]
-    if len(hexValue) > 4:
+    if int(teamID) > 16384 and int(teamID) <= 65536:
         if hexValue.startswith("4") or hexValue.startswith(
             "5"
         ):  # from ACL eFootball to ACL PES
             return str(
                 int(teamID) + 65536 - 16384  # + PES 21 ACL OFFSET - eF ACL OFFSET
             )
-        elif hexValue.startswith("10") or hexValue.startswith(
+        else:
+            return teamID
+    elif int(teamID) > 65536:
+        if hexValue.startswith("10") or hexValue.startswith(
             "11"
         ):  # from Asian Cup to Real ID (65536 is a coincidence)
             return str(int(teamID) - 65536)
